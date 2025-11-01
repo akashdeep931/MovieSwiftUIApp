761c5a89fba74d3abb91cdb1cd1aa561cb814371
akashdeep931 <akashdeepsingh931@gmail.com>
2025-11-01 17:26:55 +0000

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Akashdeep Singh Kaur on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
